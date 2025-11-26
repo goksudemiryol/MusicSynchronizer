ec5a52709037b36c370483451e1a8cd13fdda043
Goksu Demiryol <goksu.demiryol@aksis.com.tr>
2025-11-26 20:36:00 +0300

    Added README.md

--- MusicSynchronizer.Api/ViewModels/SyncResponseViewModel.cs
+++ MusicSynchronizer.Api/ViewModels/SyncResponseViewModel.cs
@@ -11,13 +11,13 @@ public class SyncResponseViewModel
 
     public Playlist SourcePlaylist { get; set; }
 
-    public List<SuccessfulSync> Successful { get; set; } = [];
-
-    public List<FailedSync> Failed { get; set; } = [];
-
     public int SuccessfulCount { get => Successful.Count; }
 
     public int FailedCount { get => Failed.Count; }
+
+    public List<SuccessfulSync> Successful { get; set; } = [];
+
+    public List<FailedSync> Failed { get; set; } = [];
 
 
     public static explicit operator SyncResponseViewModel(SyncResponse syncResponse)
--- MusicSynchronizer.Domain/Models/SyncResponse.cs
+++ MusicSynchronizer.Domain/Models/SyncResponse.cs
@@ -9,13 +9,13 @@ public class SyncResponse
 
     public Playlist SourcePlaylist { get; set; }
 
-    public List<SuccessfulSync> Successful { get; set; } = [];
-
-    public List<FailedSync> Failed { get; set; } = [];
-
     public int SuccessfulCount { get => Successful.Count; }
 
     public int FailedCount { get => Failed.Count; }
+
+    public List<SuccessfulSync> Successful { get; set; } = [];
+
+    public List<FailedSync> Failed { get; set; } = [];
 }
 
 public class SuccessfulSync
